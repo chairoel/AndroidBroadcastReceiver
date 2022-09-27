9123bf156508bf3e6aae00ae6fc82e153e5067ad
chairul <chairul.amri@ebconnect.com>
2022-09-27 12:07:36 +0700

    BroadcastReceiver Tutorial Part 2 - DYNAMIC RECEIVERS

--- app/src/main/java/com/amrul/connection/broadcastreceiverapp/MainActivity.java
+++ app/src/main/java/com/amrul/connection/broadcastreceiverapp/MainActivity.java
@@ -5,8 +5,6 @@ import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.amrul.connection.broadcastreceiverapp.R;
-
 public class MainActivity extends AppCompatActivity {
     ConnectionReceiver exampleBroadcastReceiver = new ConnectionReceiver();
 
