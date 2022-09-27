0aded10909ada94c6b0ef03e409531b96acc944b
chairul <chairul.amri@ebconnect.com>
2022-09-27 12:00:11 +0700

    BroadcastReceiver Tutorial Part 1 - STATIC RECEIVERS

--- app/src/main/java/com/amrul/connection/broadcastreceiverapp/ConnectionReceiver.java
+++ app/src/main/java/com/amrul/connection/broadcastreceiverapp/ConnectionReceiver.java
@@ -12,30 +12,13 @@ public class ConnectionReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        final ConnectivityManager connMgr = (ConnectivityManager) context
-                .getSystemService(Context.CONNECTIVITY_SERVICE);
-//
-        final android.net.NetworkInfo wifi = connMgr
-                .getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-
-        final android.net.NetworkInfo mobile = connMgr
-                .getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-
-
-        if (wifi.isAvailable() || mobile.isAvailable()) {
-            // Do something
-            Toast.makeText(context, "Network Available ", Toast.LENGTH_SHORT).show();
-
-            Log.d("Network Available ", "Flag No 1");
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Toast.makeText(context, "Boot completed", Toast.LENGTH_SHORT).show();
         }
-//        if (ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())) {
-//            boolean noConnectivity = intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
-//            if (noConnectivity) {
-//                Toast.makeText(context, "Disconnected", Toast.LENGTH_SHORT).show();
-//            } else {
-//                Toast.makeText(context, "Connected", Toast.LENGTH_SHORT).show();
-//            }
-//        }
+
+        if (ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())) {
+            Toast.makeText(context, "Connectivity changed", Toast.LENGTH_SHORT).show();
+        }
 
     }
 
